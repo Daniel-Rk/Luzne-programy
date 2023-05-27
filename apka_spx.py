091322a746b0d61c4398010d6395ae2b8d156de1
Daniel-Rk <dsprajcik@o2.p>
2023-05-27 18:28:46 +0200

    commit

---
+++ apka_spx.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import pandas as pd
+import yfinance as yf
+import plotly.express as px
+
+
+sp500 = yf.Ticker("^SPX").history(period="5y")
+st.sidebar.title('Econometrics app')
+
+
+# fig = px.line(sp500, x=sp500.index, y=["Low", "High"])
+fig = px.line(sp500, x=sp500.index, y="Close")
+fig.update_traces(textposition="bottom right")
+
+
+st.plotly_chart(fig)
+
+
+sp500
