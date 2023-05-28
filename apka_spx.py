34bce602cecdc5123dc4c1661379bf6355b4b694
Daniel-Rk <dsprajcik@o2.p>
2023-05-28 21:59:37 +0200

    NIEDZIELA END

--- apka_spx.py
+++ apka_spx.py
@@ -4,7 +4,7 @@ import yfinance as yf
 import plotly.express as px
 from finance_core import *
 from sklearn.linear_model import LinearRegression
-import plotly.graph_objects as go
+
 
 sp500 = yf.Ticker("^SPX").history(period="5y")
 st.sidebar.title('Econometrics app')
@@ -16,6 +16,7 @@ st.sidebar.write(array)
 show_average = st.sidebar.checkbox('Show average')
 show_outlier = st.sidebar.checkbox('Show outliers')
 show_linear_regres = st.sidebar.checkbox('Show linear regres')
+show_kde = st.sidebar.checkbox('Show KDE')
 # fig = px.line(sp500, x=sp500.index, y=["Low", "High"])
 fig = px.line(sp500, x=sp500.index, y="Close")
 fig.update_traces(textposition="bottom right")
@@ -37,6 +38,17 @@ df = yahoo(array).cena(start_=date)
 
 rng = np.random.RandomState(42)
 clf = IsolationForest(max_samples=100, random_state=rng)
+
+
+def kde_sklearn(x, x_grid, bandwidth=0.2, **kwargs):
+    """Kernel Density Estimation with Scikit-learn"""
+    kde_skl = KernelDensity(bandwidth=bandwidth, **kwargs)
+    kde_skl.fit(x[:, np.newaxis])
+    # score_samples() returns the log-likelihood of the samples
+    log_pdf = kde_skl.score_samples(x_grid[:, np.newaxis])
+    return np.exp(log_pdf)
+
+
 
 def plot_seperate():
     for i,j in enumerate(df):
@@ -73,6 +85,11 @@ def plot_seperate():
             df['Regres model'] = y_range
 
             fig = px.line(df, x=df.index, y=[j, 'Regres model'])
+
+
+        if show_kde is True:
+            fig_2 = px.histogram(df, x=j)
+            st.plotly_chart(fig_2)
 
         st.plotly_chart(fig)
 
